59ef4d6477d75e7f820b932e6f6d92bc819a3ea1
venkat-devish <venkatsuryakiranv@gmail.com>
2023-01-22 01:44:35 +0530

    add: future is now section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./features.scss";
+import Feature from "../../components/feature/Feature";
+import { featuresData } from "../../helpers/featuresData";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featuresData.map((feature, idx) => {
+          return (
+            <Feature
+              key={feature.title + idx}
+              title={feature.title}
+              text={feature.text}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Features;
